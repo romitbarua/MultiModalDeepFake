15edf27f62e2cc0d07e4d4bda92760458ca0d8b1
Romit Barua <romitbarua@gmail.com>
2023-03-27 15:33:15 -0700

    Draft #1 of embeddings module

---
+++ packages/AudioEmbeddingsManager.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class AudioEmbeddingsManager:
+
+    def __init__(self, model, data) -> None:
+        self.model = model
+        self.data = data
+
+    def generateEmbeddings(self, normalize: bool = False):
+
+        embeddings = np.array([self.model.get_embedding(file_path).cpu().detach().numpy()[0] for file_path in self.data['paths']])
+        
+        if normalize:
+            raise NotImplementedError
+            #embeddings = normalize(embeddings)
+        
+        np.random.shuffle(embeddings)
+        return embeddings
+
+
+
+    
