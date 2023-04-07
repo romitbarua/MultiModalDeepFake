89c1be14b51c5b93549e393835dec83f8f066072
romitbarua <romitbarua@gmail.com>
2023-04-07 22:01:43 +0000

    adding the SavedFeatureLoader

--- packages/AudioEmbeddingsManager.py
+++ packages/AudioEmbeddingsManager.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+SAVED_EMBEDDINGS_DIR = '/home/ubuntu/data/wavefake_data/Embeddings/16000KHz'
+
 def generateTitaNetEmbeddings(model, paths, normalize):
     
         embeddings = np.array([model.get_embedding(file_path).cpu().detach().numpy()[0] for file_path in paths])
@@ -11,6 +13,8 @@ def generateTitaNetEmbeddings(model, paths, normalize):
         
         return embeddings
     
+def loadSavedData(paths, saved_data_dir=SAVED_EMBEDDINGS_DIR):
+    pass
 
 class AudioEmbeddingsManager:
 
@@ -18,12 +22,17 @@ class AudioEmbeddingsManager:
         self.model = model
         self.data = data
 
-    def generateFeatureDf(self, normalize: bool = False):
+    def generateFeatureDf(self, normalize: bool = False, regenerate_embeddings: bool = False):
         
-        embeddings_df = pd.DataFrame(self.generateEmbeddings(normalize))
-        
-        feature_cols = list(embeddings_df.columns)
-        feature_df = pd.concat((self.data, embeddings_df), axis=1)
+        if regenerate_embeddings:
+            embeddings_df = pd.DataFrame(self.generateEmbeddings(normalize))
+
+            feature_cols = list(embeddings_df.columns)
+            feature_df = pd.concat((self.data, embeddings_df), axis=1)
+            
+        else:
+            pass
+            
         return feature_df, feature_cols
 
 
---
+++ packages/SavedFeatureLoader.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import os
+import random
+from tqdm import tqdm
+from pathlib import Path
+import json
+
+FEATURE_MAP_PATH = '/home/ubuntu/data/FeatureMap.json'
+
+
+def loadFeatures(metadata, feature_type, metadata_filepath_col='path', feature_filepath_col='path', feature_map_path=FEATURE_MAP_PATH):
+    
+    metadata['path_keys'] = metadata['path'].apply(os.path.dirname)
+    present_paths = metadata['path_keys'].unique().tolist()
+    with open(feature_map_path) as f:
+        feature_map = json.load(f)
+    
+    merged_df = pd.DataFrame()
+    for path in tqdm(present_paths):
+        feature_df = pd.read_csv(feature_map[path][feature_type])
+        filter_metadata = metadata[metadata['path_keys'] == path]
+        merged_df = pd.concat([merged_df,pd.merge(filter_metadata, feature_df, how='left', left_on=metadata_filepath_col, right_on=feature_filepath_col)], axis=0).reset_index(drop=True)
+        
+    return merged_df
+        
+    
+
+    
+
+        
