0b9c64971bec58e25fad313fc1d2d3a23fb31406
Romit Barua <romitbarua@gmail.com>
2023-04-23 16:38:44 -0700

    adding smile feature manager

---
+++ packages/SmileFeatureManager.py
@@ -0,0 +1,40 @@
+from cProfile import label
+from mimetypes import init
+
+
+from packages.SavedFeatureLoader import loadFeatures
+from packages.SmileFeatureSelector import *
+
+VALID_FEATURE_SELECTORS = ['random_forest']
+
+class SmileFeatureManager:
+
+    def __init__(self, data) -> None:
+        self.data = data
+        self.loadSavedFeatures()
+
+    def loadSavedFeatures(self):
+
+        self.feature_df = loadFeatures(self.data.copy(), 'openSmile',  feature_filepath_col='file')
+
+    def generateFeatureDf(self, feature_selector_type, label_type, feature_count=10):
+        
+        assert feature_selector_type in VALID_FEATURE_SELECTORS, f'{feature_selector_type} not valid. Valid types include {VALID_FEATURE_SELECTORS}'
+        assert label_type in ['binary', 'multiclass'], 'Label type must be either binary or multiclass'
+
+        if feature_selector_type == 'random_forest':
+            selector = smileFeatureSelectFromModel(self.feature_df)
+
+            if label_type == 'binary':
+                return selector.select_features_binary(max_features=feature_count, return_df=True)
+            else:
+                return selector.select_features_multiclass(max_features=feature_count, return_df=True) 
+
+
+    
+
+        
+
+
+
+
