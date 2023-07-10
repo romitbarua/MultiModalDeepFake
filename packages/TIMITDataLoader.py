009b82402343bf74cb3544812d8e9a7cf8fabf02
romitbarua <romitbarua@gmail.com>
2023-07-10 12:30:11 +0000

    UPDATED TimitDataLoader.py with new method for accessing timit files via path rather than folder

--- packages/TIMITDataLoader.py
+++ packages/TIMITDataLoader.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import pathlib
 from random import random, sample, seed, shuffle
 import pandas as pd
 import numpy as np
@@ -23,9 +24,12 @@ class TIMITDataLoader:
         
         return(cleaned_files)
         
-    def generate_split(self):
+    def generate_split(self, folder=False):
 
-        data_df = self.generateFinalDataFrame()
+        if folder:
+            data_df = self.generateFinalDataFrame_folder()
+        else:
+            data_df = self.generateFinalDataFrame()
         
         indices = list(data_df.index)
         
@@ -99,6 +103,91 @@ class TIMITDataLoader:
         
     
     def generateFinalDataFrame(self, balanced: bool = True):
+    
+        # Get resampled real and fake files
+
+        all_wav_files = pathlib.Path(self.file_path)
+        all_wav_files = list(all_wav_files.rglob("*.wav")) + list(all_wav_files.rglob("*.WAV"))
+
+        real_resampled_wav_files = [str(file) for file in all_wav_files if 'real' in str(file)]
+        fake_resampled_wav_files = [str(file) for file in all_wav_files if 'fake/' in str(file)]
+
+
+        # Get rid of folders that contain any wrong files
+        final_folders = []
+
+        for folder in os.listdir(self.file_path):
+            phrase_files = [phrase for phrase in real_resampled_wav_files if folder in phrase]
+
+            file_names = set([name.split('_')[-1].split('.')[0] for name in phrase_files])
+
+            if len(file_names) > 1:
+                continue
+
+            # Ensure each file has at least 2 real samples
+            elif len(phrase_files) > 1:
+                final_folders.append(folder)
+
+        print(len(final_folders))
+
+        real_files = []
+        fake_files = []
+
+        print(f'Params: {len(final_folders)} different phrases')
+
+        # Temporary: remove duplicate files
+        file_dict = {}
+        for i in range(len(real_resampled_wav_files)):
+            file_name = real_resampled_wav_files[i].split('/')[-1]
+            file_dict[file_name] = real_resampled_wav_files[i]
+
+        real_resampled_wav_files = [file_dict[item] for item in file_dict.keys()]
+
+
+        for n in range(len(final_folders)):
+            phrase = final_folders[n]
+
+            real_examples = [file for file in real_resampled_wav_files if f'_{phrase}.' in file]
+            real_examples = [file for file in real_resampled_wav_files if f'/{phrase}/' in file]
+
+            fake_examples = [file for file in fake_resampled_wav_files if f'_{phrase}.' in file]
+            fake_examples = [file for file in fake_resampled_wav_files if f'/{phrase}/' in file]
+
+            # Ensure we take the same number of each phrase for real and fake, downsample the fake files 
+
+            if len(real_examples) > len(fake_examples):
+                real_examples = sample(real_examples, len(fake_examples))
+            else:
+                fake_examples = sample(fake_examples, len(real_examples))
+
+            [real_files.append(file) for file in real_examples]
+            [fake_files.append(file) for file in fake_examples]
+
+
+
+        balanced_real_paths = real_files
+        balanced_fake_paths = fake_files
+
+
+        df = pd.DataFrame({'type':['tbc' for i in range(len(balanced_real_paths)+len(balanced_fake_paths))],
+                          'id':[i for i in range(len(balanced_real_paths)+len(balanced_fake_paths))], 
+                          'architecture':[0 for item in balanced_real_paths] + [1 for item in balanced_fake_paths],
+                           'orig_path': balanced_real_paths + balanced_fake_paths,
+                           'label':[0 for item in balanced_real_paths] + [1 for item in balanced_fake_paths],
+                           'multiclass_label':[0 for item in balanced_real_paths] + [1 for item in balanced_fake_paths]
+                          })
+
+
+        downsampled_src = '/home/ubuntu/data/TIMIT_and_ElevenLabs/16KHz'
+        orig_paths = df['orig_path'].tolist()
+        downsampled_paths = [os.path.join(downsampled_src, os.path.basename(path)) for path in orig_paths]
+
+        df['path'] = downsampled_paths
+
+
+        return df
+    
+    def generateFinalDataFrame_folder(self, balanced: bool = True):
         
         phrases = [item for item in os.listdir(self.file_path) if not item.startswith('.')]
         
